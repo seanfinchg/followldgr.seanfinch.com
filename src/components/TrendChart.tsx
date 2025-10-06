3e5ab3178bde0bfc325b404cae665a6765399adc
seanfinchg <seanfinchg@gmail.com>
2025-10-06 02:10:03 -0400

    fix build

--- src/components/TrendChart.tsx
+++ src/components/TrendChart.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Box,
-  Typography,
-  Card,
-  CardContent,
-} from "@mui/material";
+import { Box, Typography, Card, CardContent } from "@mui/material";
 import type { Snapshot, BasicUser } from "../types";
 
 // Helper function to calculate order index statistics
